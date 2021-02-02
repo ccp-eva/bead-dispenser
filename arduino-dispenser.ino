563e08bb7d5372140cc75f93d2d82c1dbb2bb9fd
Steven Kalinke <steven.kalinke@gmail.com>
2021-02-02 14:49:21 +0100

    Remove LED Parts for now

--- arduino-dispenser.ino
+++ arduino-dispenser.ino
@@ -2,16 +2,9 @@
 // ------------------------------------------------------------------------------------------------------
 // Resources:
 // - Adjusted Example 4 from: https://forum.arduino.cc/index.php?topic=396450.msg2727728#msg2727728
-//   - Use "No line ending" in Serial Monitor (to avoid double prints)
 // - Extend by the Arduino IR Breakbeam example: https://learn.adafruit.com/ir-breakbeam-sensors/arduino
 // ------------------------------------------------------------------------------------------------------
 
-
-
-
-// Adafruit includes (todo: is Wire.h needed? Or just for non-standard calls such as I2C: Adafruit_MotorShield(0x61))
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
 
 
 // =============================================
@@ -37,12 +30,10 @@ bool newData = false;
 int dataNumber = 0; 
 
  
-void setup() {
-  // initialize the LED pin as an output: // can be removed later
-  pinMode(LEDPIN, OUTPUT);      
+void setup() {    
   // initialize the sensor pin as an input:
   pinMode(SENSORPIN, INPUT);     
-  digitalWrite(SENSORPIN, HIGH); // turn on the pullup
+  digitalWrite(SENSORPIN, HIGH);
   
   Serial.begin(9600);
   Serial.println("Arduino Ready");
@@ -52,6 +43,7 @@ void loop(){
 
   // check & receive new user input, newline should be used as EOL (LF)
   if (Serial.available() > 0) {
+      // get serial input
       recvWithEndMarker();
       // cast input to int
       dataNumber = atoi(receivedChars);
@@ -62,35 +54,6 @@ void loop(){
       newData = false;
   }
   
-  
-  
-
-  
-
- 
-  
-
-
-
-
-  // LED part
-  // ------ CAN BE REMOVED LATER ------
-  // check if the sensor beam is broken
-  // if it is, the currentSensorState is LOW:
-  if (currentSensorState == LOW) {     
-    // turn LED on to indicate broken beam:
-    digitalWrite(LEDPIN, HIGH);  
-  } 
-  else {
-    // turn LED off to indicate unbroken beam:
-    digitalWrite(LEDPIN, LOW); 
-  }
-  // ------ CAN BE REMOVED LATER ------
-
-
-
-
-
 
 
   while (isCounting) {
@@ -112,11 +75,7 @@ void loop(){
     }
     // --------------------------------------------------------------------------------------------------
   
-
-    // Stepper Motor Algorithm
-    
     Serial.println(counter);
-  
     
     // synchronize states (this should be at the end)
     lastSensorState = currentSensorState;
@@ -129,8 +88,6 @@ void loop(){
       counter = 0;
     }
   }
-
-
 }
 
 
