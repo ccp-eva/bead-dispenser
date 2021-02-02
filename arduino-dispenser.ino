12c2708a1c494f8519f343c02c3fcd5da6ace59c
Steven Kalinke <steven.kalinke@gmail.com>
2021-02-02 17:41:15 +0100

    Fix New Line Error

--- arduino-dispenser.ino
+++ arduino-dispenser.ino
@@ -1,19 +1,15 @@
-
 // ------------------------------------------------------------------------------------------------------
 // Resources:
 // - Adjusted Example 4 from: https://forum.arduino.cc/index.php?topic=396450.msg2727728#msg2727728
+//   - make sure to set EOL to Newline (LF)
 // - Extend by the Arduino IR Breakbeam example: https://learn.adafruit.com/ir-breakbeam-sensors/arduino
 // ------------------------------------------------------------------------------------------------------
 
-
-
 // =============================================
-// IR PART
+// IR BREAKBEAM PART
 // =============================================
 // IR Breakbeam Sensor Definitions
-#define LEDPIN 13  // can be removed later
 #define SENSORPIN 4
- 
 // IR breakbeam sensor state variables
 int currentSensorState = 0, lastSensorState = 0; // either LOW or HIGH (i.e., 0 or 1. NB: LOW means beam is broken, HIGH means unbroken)
 
@@ -21,43 +17,65 @@ int currentSensorState = 0, lastSensorState = 0; // either LOW or HIGH (i.e., 0 
 int counter = 0;
 bool isCounting = false;
 
-
 // =============================================
 // SERIAL INPUT PART
 // =============================================
-char receivedChars[32];
+char receivedChars[32]; // an array to store the received data
 bool newData = false;
-int dataNumber = 0; 
+int dataNumber = 0;
 
- 
-void setup() {    
+void setup() {
   // initialize the sensor pin as an input:
-  pinMode(SENSORPIN, INPUT);     
+  pinMode(SENSORPIN, INPUT);
   digitalWrite(SENSORPIN, HIGH);
-  
   Serial.begin(9600);
   Serial.println("Arduino Ready");
 }
- 
-void loop(){
 
-  // check & receive new user input, newline should be used as EOL (LF)
-  if (Serial.available() > 0) {
-      // get serial input
-      recvWithEndMarker();
-      // cast input to int
-      dataNumber = atoi(receivedChars);
+void loop() {
 
-      // enable counting mode
-      isCounting = true;
+  // get serial input
+  recvWithEndMarker();
 
-      newData = false;
+  if (newData == true) {
+    initMotor();
   }
-  
 
+}
+
+void recvWithEndMarker() {
+  static byte ndx = 0;
+  char endMarker = '\n';
+  char rc;
+
+  if (Serial.available() > 0) {
+    rc = Serial.read();
+
+    if (rc != endMarker) {
+      receivedChars[ndx] = rc;
+      ndx++;
+      if (ndx >= 32) {
+        ndx = 32 - 1;
+      }
+    } else {
+      receivedChars[ndx] = '\0'; // terminate the string
+      ndx = 0;
+      newData = true;
+    }
+  }
+}
+
+void initMotor() {
+  dataNumber = 0;
+  // cast char to int
+  dataNumber = atoi(receivedChars);
+  Serial.print("Counting to ... ");
+  Serial.println(dataNumber);
+
+  // start counting mode
+  isCounting = true;
 
   while (isCounting) {
-
     // --------------------------------------------------------------------------------------------------
     // Sensor Algorithm
     // --------------------------------------------------------------------------------------------------
@@ -66,17 +84,17 @@ void loop(){
     // if there is a diff in currentSensorState and lastSensorState
     if (currentSensorState == HIGH && lastSensorState == LOW) {
       Serial.println("Unbroken");
-    } 
+    }
     if (currentSensorState == LOW && lastSensorState == HIGH) {
       Serial.println("Broken");
-      
+
       // if beam is broken we can assume that an object is blocking it, so incrementing the counter
       counter += 1;
     }
     // --------------------------------------------------------------------------------------------------
-  
+
     Serial.println(counter);
-    
+
     // synchronize states (this should be at the end)
     lastSensorState = currentSensorState;
 
@@ -88,27 +106,6 @@ void loop(){
       counter = 0;
     }
   }
-}
 
-
-
-void recvWithEndMarker() {
-  static byte ndx = 0;
-  char endMarker = '\n';
-  char rc;
- 
-  rc = Serial.read();
-
-  if (rc != endMarker) {
-      receivedChars[ndx] = rc;
-      ndx++;
-      if (ndx >= 32) {
-          ndx = 32 - 1;
-      }
-  }
-  else {
-      receivedChars[ndx] = '\0'; // terminate the string
-      ndx = 0;
-      newData = true;
-  }
+  newData = false;
 }
